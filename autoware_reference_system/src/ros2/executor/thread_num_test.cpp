6facb965c8e24ccbe44134adb97c1686d46bdf31
ruoxianglee <ruoxianglee@163.com>
2024-01-05 11:30:54 +0800

    number of threads testing

--- autoware_reference_system/src/ros2/executor/thread_num_test.cpp
+++ autoware_reference_system/src/ros2/executor/thread_num_test.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char * argv[])
 {
-    for (int i = 0; i++; i < 1000)
+    for (int i = 0; i < 1000; i++)
     {
         unsigned int n = std::thread::hardware_concurrency();
         std::cout << n << " concurrent threads are supported.\n";
