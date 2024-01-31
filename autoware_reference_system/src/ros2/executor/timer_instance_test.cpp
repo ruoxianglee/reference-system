3646859508864772138b589b66690a478f7c6793
ruoxianglee <ruoxianglee@163.com>
2024-01-31 12:57:45 +0800

    debug

--- autoware_reference_system/src/ros2/executor/timer_instance_test.cpp
+++ autoware_reference_system/src/ros2/executor/timer_instance_test.cpp
@@ -8,8 +8,61 @@
 #include "autoware_reference_system/system/timing/default.hpp"
 #include <chrono>
 
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "std_msgs/msg/string.hpp"
+
+#include<unistd.h>  
+
+using namespace std::chrono_literals;
+
 // using time_t = std::chrono::nanoseconds;
 // using milliseconds = std::chrono::milliseconds;
+
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+  : Node("minimal_publisher"), count_(0)
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    timer_ = this->create_wall_timer(
+      5ms, std::bind(&MinimalPublisher::timer_callback, this));
+  }
+
+private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, world! " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+  MinimalSubscriber()
+  : Node("minimal_subscriber")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const std_msgs::msg::String & msg) const
+  {
+    usleep(50000); // 50 ms
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
 
 int main(int argc, char * argv[])
 {
